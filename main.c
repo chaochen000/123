b25d76d9e7f8ae4c2a31a6c39629416881385d32
chaochen000 <39374617+chaochen000@users.noreply.github.com>
2018-05-18 03:19:21 +0800

    37

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a , b ,c ;
+	printf("¿é¤Ja\n");
+	scanf("%d",&a);
+	printf("¿é¤Jb\n");
+	scanf("%d",&b);
+	c=a + b ;
+	printf("a + b = %d",c);
+	return 0;
+}
